333407418d06328ff4363ea35135cbd080d4b2f5
Md. Razu Haolader <razu.cse129@gmail.com>
2023-03-25 21:43:14 +0600

    fix form update

--- includes/Custom/views/book-pub-info-meta-form.php
+++ includes/Custom/views/book-pub-info-meta-form.php
@@ -4,6 +4,6 @@ wp_nonce_field(basename(__FILE__), 'book_publications_field');
 ?>
 
 <label for="mrhbr_book_publisher"><?php _e('Book Publications', MRHBR_DOMAIN); ?></label>
-<input type="text" name="mrhbr_book_publisher" id="mrhbr_book_publisher" class="postbox" value="<?php echo $publisher; ?>">
+<input type="text" name="mrhbr_book_publisher" id="mrhbr-book-publisher" class="postbox" value="<?php echo $publisher; ?>">
 <label for="mrhbr_book_publish_date"><?php _e('Book Publish Date', MRHBR_DOMAIN); ?></label>
-<input type="date" name="mrhbr_book_publish_date" id="mrhbr_book_publish_date" class="postbox" value="<?php echo $publish_date; ?>">
+<input type="date" name="mrhbr_book_publish_date" id="mrhbr-book-publish_date" class="postbox" value="<?php echo $publish_date; ?>">
