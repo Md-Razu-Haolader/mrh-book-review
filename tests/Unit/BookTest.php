300da16bac69033b83b29a3a8b79d369aef37a46
Md. Razu Haolader <razu.cse129@gmail.com>
2023-04-11 12:25:45 +0600

    Update test case

--- tests/Unit/BookTest.php
+++ tests/Unit/BookTest.php
@@ -1,25 +1,22 @@
 <?php
 
-declare(strict_types=1);
+declare( strict_types=1 );
 
 namespace MRH\BookReview\Tests\Unit;
 
 use MRH\BookReview\Custom\PostTypes\Book;
 use Yoast\PHPUnitPolyfills\TestCases\TestCase;
 
-class BookTest extends TestCase
-{
+class BookTest extends TestCase {
 
     private static $book;
 
-    public static function set_up_before_class(): void
-    {
+    public static function set_up_before_class(): void {
         static::$book = new Book();
     }
 
-    public function test_should_add_book_post_type()
-    {
+    public function test_should_add_book_post_type() {
         static::$book->add_post_type();
-        $this->assertTrue(post_type_exists(MRHBR_POST_TYPE));
+        $this->assertTrue( post_type_exists( MRHBR_POST_TYPE ) );
     }
 }
